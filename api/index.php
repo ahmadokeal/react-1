a8bdff8744ee705fa6ef0cfd5122e754665b16bb
ahmad okeal <104964555+ahmadokeal@users.noreply.github.com>
2023-06-18 18:03:46 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: https://react-1-plum-nine.vercel.app');
 // if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  echo file_get_contents(__DIR__ . '/MOCK_DATA.json');
+  echo file_get_contents('./MOCK_DATA.json');
 // }
